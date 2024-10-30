cec390eadf6fa3ad71a238e2cdca9ee173be3c63
Giorgio Mattina <131525010+Gio-lly@users.noreply.github.com>
2024-10-30 16:59:41 +0100

    optimized index.js

--- classroom_code/buffer_fun/index.js
+++ classroom_code/buffer_fun/index.js
@@ -1,36 +1,27 @@
 
 const c = new AudioContext();
-
-function createsANoiseBuffer() {
-    const b = c.createBuffer(1, c.sampleRate * 2, 
-    c.sampleRate);
-
-    const audioData = b.getChannelData(0);
-
-    for(var i=0; i<audioData.length; i++) {
-        audioData[i] = Math.random();   
-    }
-    return b;
-}
-
-function createsASineBuffer(f) {
-        return function() {
+//sus is boolean(true = sine, false  = noise)
+function createABuffer(sus,freq){
+     return function() {
+        //create by default a buffer
         const b = c.createBuffer(1, c.sampleRate * 2, 
         c.sampleRate);
-
-        
         alpha = Math.PI*2*f/c.sampleRate;
         const audioData = b.getChannelData(0);
-
+         //if !sus, fill it with nois and return
+        if(!sus){
+            for(var i=0; i<audioData.length; i++) {
+            audioData[i] = Math.random();   
+            }
+         return b;
+        }
+        //else fill it with sine
         for(var i=0; i<audioData.length; i++) {
             audioData[i] = Math.sin(alpha*i);   
         }
         return b;
     }
 }
-
-
-
 function playBuffer(bufferCreator) {
 
     b = bufferCreator();
